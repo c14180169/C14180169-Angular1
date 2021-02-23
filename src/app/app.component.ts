0869f7271ea98fab9f7e12a9ba0b365d9e9abdf6
c14180169 <c14180169@john.petra.ac.id>
2021-02-23 18:43:57 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,11 @@ export class AppComponent  {
   name = 'Angular ' + VERSION.major;
   turn = 1;
   player = '0';
+  win = false;
   instruction = "GAME TURN -> ";
   instruction1 = "PLAYER TURN -> ";
-  instruction3 = "PLAYER" + this.player + "WIN";
+  instruction3 = "PLAYER " + this.player + " WIN";
+  instruction4 = "";
 
   board = [
     ['*', '*', '*', '*', '*'],
@@ -37,16 +39,55 @@ export class AppComponent  {
     if(this.input1 != "" && this.input2 != ""){
       var temp1 = +this.input1;
       var temp2 = +this.input2;
-      this.board[temp1][temp2] = this.player;
-    
+      if(this.board[temp1][temp2] != '*'){
+         this.instruction4 = "COORDINATE OCCUPIED";
+      }
+      else if(temp1 > 5 || temp2 > 5){
+        this.instruction4 = "INPUT FIELD CANT MORE THAN 5";
+      }
+      else{
+        this.board[temp1][temp2] = this.player;
+        
+
+        this.checkWin();
+        this.turn++;
+        this.changePlayer();
+      }
+      
       this.input1 = "";
       this.input2 = "";
-
-      this.turn++;
-      this.changePlayer();
+      this.instruction4 = "";
     }
     else {
-      this.instruction = "INPUT FIELD STILL EMPTY...";
+      this.instruction4 = "INPUT FIELD STILL EMPTY...";
+    }
+  }
+
+  checkWin(){
+    for(var i=0;i<5;i++){
+      var score0 = 0;
+      var score1 = 0;
+      var score00 = 0;
+      var score11 = 0;
+
+      for(var j=0;j<5;j++){
+        if(this.board[i][j] == '0'){
+          score0++;
+        }
+        else if(this.board[i][j] == '1'){
+          score1++;
+        }
+
+        if(this.board[j][i] == '0'){
+          score00++;
+        }
+        else if(this.board[j][i] == '1'){
+          score11++;
+        }
+      }
+      if(score0 == 4 || score1 == 4 || score00 == 4 || score11 == 4){
+        this.win = true;
+      }
     }
   }
 
