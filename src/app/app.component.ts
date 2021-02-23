6fe0ee4cd2c22a119072ca5548df3c585c18050a
c14180169 <c14180169@john.petra.ac.id>
2021-02-23 18:27:11 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,50 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  turn = 1;
+  player = '0';
+  instruction = "GAME TURN -> ";
+  instruction1 = "PLAYER TURN -> ";
+  instruction3 = "PLAYER" + this.player + "WIN";
+
+  board = [
+    ['*', '*', '*', '*', '*'],
+    ['*', '*', '*', '*', '*'],
+    ['*', '*', '*', '*', '*'],
+    ['*', '*', '*', '*', '*'],
+    ['*', '*', '*', '*', '*']
+    ];
+
+  input1 = "";
+  input2 = "";
+
+  changePlayer(){
+    if(((this.turn - 1) % 2) == 0) {
+      this.player = '0';
+    }
+    else{
+      this.player = '1';
+    }
+  }
+
+  pickAnswer(){
+    if(this.input1 != "" && this.input2 != ""){
+      var temp1 = +this.input1;
+      var temp2 = +this.input2;
+      this.board[temp1][temp2] = this.player;
+    
+      this.input1 = "";
+      this.input2 = "";
+
+      this.turn++;
+      this.changePlayer();
+    }
+    else {
+      this.instruction = "INPUT FIELD STILL EMPTY...";
+    }
+  }
+
+  
+
+  
 }
